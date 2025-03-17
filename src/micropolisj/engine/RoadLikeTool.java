e55945d31e98cbd67be3dba568bfe21f4f96ae43
koemi-01 <1246909122@qq.com>
2025-03-16 23:12:08 -0400

    Fix for bug F: building underwater wires costs the wrong amount
    Found bug in laying wires under water, should be using the wire cost under water.
    Fixed bug to use the right cost.

--- src/micropolisj/engine/RoadLikeTool.java
+++ src/micropolisj/engine/RoadLikeTool.java
@@ -360,6 +360,65 @@ class RoadLikeTool extends ToolStroke
 		switch (tile)
 		{
 		case RIVER:		// wire on water
+			cost = UNDERWATER_WIRE_COST;
+			{
+				int tmp = eff.getTile(1, 0);
+				char tmpn = neutralizeRoad(tmp);
+
+				if (isConductive(tmp) &&
+					tmpn != HROADPOWER &&
+					tmpn != RAILHPOWERV &&
+					tmpn != HPOWER)
+				{
+					eff.setTile(0, 0, VPOWER);
+					break;
+				}
+			}
+
+			// check west
+			{
+				int tmp = eff.getTile(-1, 0);
+				char tmpn = neutralizeRoad(tmp);
+
+				if (isConductive(tmp) &&
+					tmpn != HROADPOWER &&
+					tmpn != RAILHPOWERV &&
+					tmpn != HPOWER)
+				{
+					eff.setTile(0, 0, VPOWER);
+					break;
+				}
+			}
+
+			// check south
+			{
+				int tmp = eff.getTile(0, 1);
+				char tmpn = neutralizeRoad(tmp);
+
+				if (isConductive(tmp) &&
+					tmpn != VROADPOWER &&
+					tmpn != RAILVPOWERH &&
+					tmpn != VPOWER)
+				{
+					eff.setTile(0, 0, HPOWER);
+					break;
+				}
+			}
+
+			// check north
+			{
+				int tmp = eff.getTile(0, -1);
+				char tmpn = neutralizeRoad(tmp);
+
+				if (isConductive(tmp) &&
+					tmpn != VROADPOWER &&
+					tmpn != RAILVPOWERH &&
+					tmpn != VPOWER)
+				{
+					eff.setTile(0, 0, HPOWER);
+					break;
+				}
+			}
 		case REDGE:
 		case CHANNEL:
 
